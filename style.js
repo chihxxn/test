b235bc769964113d08f540e3f57474f20485bed4
chihxxn <153482469+chihxxn@users.noreply.github.com>
2023-12-11 17:17:40 +0900

    Update and rename test_java.js to style.js

---
+++ style.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // 페이지가 로드된 후 실행할 코드
+
+    // 애니메이션 효과를 위한 클래스 추가
+    document.querySelector(".animation-container").classList.add("animate-in");
+});
