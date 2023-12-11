ecc3cd5e0972c24ff06de76b3966de6c982fb727
chihxxn <153482469+chihxxn@users.noreply.github.com>
2023-12-11 17:40:09 +0900

    Update style.js

--- style.js
+++ style.js
@@ -4,3 +4,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // 애니메이션 효과를 위한 클래스 추가
     document.querySelector(".animation-container").classList.add("animate-in");
 });
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // 페이지가 로드된 후 실행할 코드
+
+    // 애니메이션 효과를 위한 클래스 추가
+    document.querySelector(".animated-title").classList.add("animate-in");
+    document.querySelector(".animated-image").classList.add("animate-in");
+    document.querySelectorAll(".animated-text").forEach(function(text) {
+        text.classList.add("animate-in");
+    });
+
+    // 나머지 코드는 그대로 유지
+});
